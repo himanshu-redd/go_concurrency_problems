5ed1f427be4ef64c5b168921fe8949a208917789
Himanshu Ranjan <himanshu.red98@gmail.com>
2025-06-21 17:48:26 +0530

    refactor ex10

--- beginner/ex10/solve.go
+++ beginner/ex10/solve.go
@@ -3,6 +3,7 @@ package ex10
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
 func Solve() {
@@ -11,21 +12,26 @@ func Solve() {
 
 	wg.Add(2)
 
-	go produce(&wg, ch, 1)
+	go produce(&wg, ch)
 	go consume(&wg, ch)
 
 	wg.Wait()
 }
 
-func produce(wg *sync.WaitGroup, ch chan int, val int) {
+func produce(wg *sync.WaitGroup, ch chan int) {
 	defer wg.Done()
-	ch <- 1
+	for i := 1; i <= 10; i++ {
+		fmt.Println("Sent: ", i) 
+		ch <- i
+		time.Sleep(time.Duration(100) * time.Millisecond)
+	}
+	close(ch)
+
 }
 
 func consume(wg *sync.WaitGroup, ch chan int) {
 	defer wg.Done()
-	select{
-	case val := <- ch:
-		fmt.Println("chan value received: ", val)
+	for val := range ch {
+		fmt.Println("Received: ", val)
 	}
 }
